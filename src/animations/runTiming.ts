740a1b97922b440e41eb19ff683c36c198c61b64
Terry Sahaidak <s_tasssik_taras@ukr.net>
2019-10-17 21:17:04 +0300

    Fix wrong import of maybeProc

--- src/animations/runTiming.ts
+++ src/animations/runTiming.ts
@@ -4,7 +4,7 @@ import {
   updateStateProc,
 } from './../common';
 import A, { Easing } from 'react-native-reanimated';
-import { maybeProc } from 'src/utils/maybeProc';
+import { maybeProc } from '../utils/maybeProc';
 
 export interface TimingAnimationConfig {
   duration: number;
